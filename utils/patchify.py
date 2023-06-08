b87429052fa604faddcf4d3c52e4b27da2cb3a5b
Jun-WFI-hyung <shyainkr@gmail.com>
2023-06-08 14:28:14 +0900

    add flatten function in patchify

--- utils/patchify.py
+++ utils/patchify.py
@@ -1,4 +1,5 @@
-import cv2, torch, numpy as np
+import cv2, torch
+import numpy as np
 from matplotlib import pyplot as plt
 
 
@@ -19,11 +20,30 @@ def patchify(img:np.array, N:int) -> torch.Tensor:
     return x
 
 
-if __name__ == "__main__":
-    img = cv2.imread("./download.jpeg")
-    test = patchify(img=img, N=3).numpy().astype(np.uint8)
+def flatten(img:torch.Tensor) -> torch.Tensor:
+    return img.reshape(img.shape[0], -1)
 
-    for i in range(9):
-        plt.subplot(3,3,i+1)
-        plt.imshow(cv2.cvtColor(test[i, ...], cv2.COLOR_BGR2RGB))
-    plt.show()
+    
+
+if __name__ == "__main__":
+    is_image = 0
+    devide = 3
+    
+    if is_image:
+        img = cv2.imread("./download.jpeg")
+        print(img.shape)
+        test = patchify(img=img, N=devide).numpy().astype(np.uint8)
+
+        for i in range(test.shape[0]):
+            plt.subplot(3,3,i+1)
+            plt.imshow(cv2.cvtColor(test[i, ...], cv2.COLOR_BGR2RGB))
+        plt.show()
+        
+    else:
+        test_mat = np.array([[[i]*3 for i in range(1+10*j, 10+10*j)] for j in range(1, 10)], dtype=np.uint8)        
+        test = patchify(img=test_mat, N=devide)
+        print(test.shape)
+        
+        test = flatten(test)
+        print(test)
+    
